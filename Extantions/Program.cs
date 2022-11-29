06028f77129b3454b29e7bb73e39d884951df5ff
студент2 <2@DESKTOP-QEFI0G7>
2022-11-29 11:44:32 +0300

    Добавьте файлы проекта.

---
+++ Extantions/Program.cs
@@ -0,0 +1,94 @@
+﻿Console.WriteLine("введите номер телефона");
+string k = Console.ReadLine();
+int g = int.Parse (k) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////Console.WriteLine("Введите число");
+////////////string g = Console.ReadLine();
+////////////int j = int.Parse(g);
+////////////int a = j;
+////////////if (j % 2 == 0)
+////////////{
+////////////    Console.WriteLine("Число целое");
+////////////}
+////////////else
+////////////{
+////////////    Console.WriteLine("Число не целое");
+////////////}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////Console.WriteLine("Введите предложение");
+////////////string s = Console.ReadLine();
+////////////Console.WriteLine("введите какую букву надо считать");
+////////////string b = Console.ReadLine();
+////////////char a = char.Parse(b);
+////////////char c = a;
+
+////////////int i = s.CharCount(c);
+////////////Console.WriteLine(i);
+////////////if (i >= 3) 
+////////////    {
+////////////    Console.WriteLine("Буква встречается часто");
+////////////    }
+////////////else if (i <= 3)
+////////////{
+////////////    Console.WriteLine("Буква встречается редко");
+////////////}
+////////////public static class StringExtension
+////////////{
+////////////    public static int CharCount(this string str, char c)
+////////////    {
+////////////        int counter = 0;
+////////////        for (int i = 0; i < str.Length; i++)
+////////////        {
+////////////            if (str[i] == c)
+////////////                counter++;
+////////////        }
+////////////        return counter;
+
+////////////    }
+
+////////////}
